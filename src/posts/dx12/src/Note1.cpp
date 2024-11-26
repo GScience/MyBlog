fc1c44be4d584d69d607997170de1cb4bafb5bad
GScience <GM2000@outlook.com>
2024-11-26 11:43:30 +0800

    确保资源释放

--- src/posts/dx12/src/Note1.cpp
+++ src/posts/dx12/src/Note1.cpp
@@ -16,7 +16,7 @@ struct DxContext {
 	ComPtr<ID3D12DescriptorHeap> rtvHeap;
 	ComPtr<ID3D12CommandAllocator> commandAllocator;
 };
-DxContext ctx{ 0 };
+std::unique_ptr<DxContext> ctx;
 
 void EnableDebugLayer() {
 	ComPtr<ID3D12Debug> debugController;
@@ -37,6 +37,7 @@ int main() {
 
 	// 创建窗体
 	Window window(500, 500, L"Note 1");
+	ctx = std::make_unique<DxContext>();
 
 	// 启用调试层
 #if defined(_DEBUG)
@@ -47,21 +48,21 @@ int main() {
 	// 创建设备
 	ThrowIfFailed(CreateDXGIFactory2(
 		dxgiFactoryFlags, 
-		IID_PPV_ARGS(&ctx.factory)));
+		IID_PPV_ARGS(&ctx->factory)));
 	ThrowIfFailed(GetHardwareAdapter(
-		ctx.factory.Get(), 
-		&ctx.adapter));
+		ctx->factory.Get(), 
+		&ctx->adapter));
 	ThrowIfFailed(D3D12CreateDevice(
-		ctx.adapter.Get(),
+		ctx->adapter.Get(),
 		D3D_FEATURE_LEVEL_12_0,
-		IID_PPV_ARGS(&ctx.device)
+		IID_PPV_ARGS(&ctx->device)
 	));
 
 	// 创建命令队列
 	D3D12_COMMAND_QUEUE_DESC queueDesc = {};
 	queueDesc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
 	queueDesc.Type = D3D12_COMMAND_LIST_TYPE_DIRECT;
-	ThrowIfFailed(ctx.device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&ctx.commandQueue)));
+	ThrowIfFailed(ctx->device->CreateCommandQueue(&queueDesc, IID_PPV_ARGS(&ctx->commandQueue)));
 
 	// 创建交换链
 	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
@@ -73,17 +74,17 @@ int main() {
 	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
 	swapChainDesc.SampleDesc.Count = 1;
 	ComPtr<IDXGISwapChain1> swapChain1;
-	ThrowIfFailed(ctx.factory->CreateSwapChainForHwnd(
-		ctx.commandQueue.Get(),
+	ThrowIfFailed(ctx->factory->CreateSwapChainForHwnd(
+		ctx->commandQueue.Get(),
 		window.GetHandle(),
 		&swapChainDesc,
 		nullptr,
 		nullptr,
 		&swapChain1
 	));
-	ThrowIfFailed(swapChain1.As(&ctx.swapChain));
+	ThrowIfFailed(swapChain1.As(&ctx->swapChain));
 	// 禁止全屏切换
-	ThrowIfFailed(ctx.factory->MakeWindowAssociation(window.GetHandle(), DXGI_MWA_NO_ALT_ENTER));
+	ThrowIfFailed(ctx->factory->MakeWindowAssociation(window.GetHandle(), DXGI_MWA_NO_ALT_ENTER));
 
 	// 创建描述符堆
 	{
@@ -92,22 +93,23 @@ int main() {
 		rtvHeapDesc.NumDescriptors = FrameCount;
 		rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
 		rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
-		ThrowIfFailed(ctx.device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&ctx.rtvHeap)));
+		ThrowIfFailed(ctx->device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&ctx->rtvHeap)));
 	}
-	UINT rtvDescriptorSize = ctx.device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	UINT rtvDescriptorSize = ctx->device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
 
 	// 创建RTV
 	{
-		CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(ctx.rtvHeap->GetCPUDescriptorHandleForHeapStart());
+		CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(ctx->rtvHeap->GetCPUDescriptorHandleForHeapStart());
 
 		// 为每一个RT创建RTV
 		for (UINT n = 0; n < FrameCount; n++)
 		{
-			ThrowIfFailed(ctx.swapChain->GetBuffer(n, IID_PPV_ARGS(&ctx.renderTargets[n])));
-			ctx.device->CreateRenderTargetView(ctx.renderTargets[n].Get(), nullptr, rtvHandle);
+			ThrowIfFailed(ctx->swapChain->GetBuffer(n, IID_PPV_ARGS(&ctx->renderTargets[n])));
+			ctx->device->CreateRenderTargetView(ctx->renderTargets[n].Get(), nullptr, rtvHandle);
 			rtvHandle.Offset(1, rtvDescriptorSize);
 		}
 	}
-	ThrowIfFailed(ctx.device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&ctx.commandAllocator)));
+	ThrowIfFailed(ctx->device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&ctx->commandAllocator)));
 	window.MainLoop(Update, Render);
+	ctx = nullptr;
 }
